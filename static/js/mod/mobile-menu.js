816d8c72be6d78795f48848dc2a571fc710e6b56
supermeme19991012 <supermeme1012@gmail.com>
2023-04-08 11:13:19 -0700

    Django-Fullstack

---
+++ static/js/mod/mobile-menu.js
@@ -0,0 +1,26 @@
+define([
+    'jquery' //requires jquery
+], function( $ ) {
+
+    var MobileMenuExport = function(menu) {
+        this.menu = $(menu); //menu container
+        this.menuBtn = $('.mobile-toggle'); // toggle dark mode icon
+        this.init();
+    };
+
+    MobileMenuExport.prototype = {
+        init: function(){
+            var self = this;
+            self.menu.addClass('nav-menu-on');
+            self.button = $('<div class="menu-button"><i class="icon icon-reorder"></i><span>Menu</span></div>');
+            self.button.insertBefore(self.menuBtn);
+            self.button.on( 'click', function(){
+                self.menu.toggleClass('active');
+                self.button.toggleClass('active');
+            });
+        }
+    };
+
+    // Export a single instance of our module:
+    return new MobileMenuExport('[role="banner"] [role="navigation"]');
+});
